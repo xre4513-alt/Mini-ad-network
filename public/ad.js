f741bf004c1b86f51cff0bd90129bf724bc2a11d
xre4513-alt <xre4513@gmail.com>
2026-04-28 22:15:43 +0530

    ad.js

--- public/ad.js
+++ public/ad.js
@@ -1,12 +1,14 @@
 (function () {
-  fetch("/getAd")
+  const API = "https://mini-ad-network.onrender.com";
+
+  fetch(API + "/getAd")
     .then(res => res.json())
     .then(ad => {
       const adDiv = document.getElementById("ad-slot");
       if (!adDiv) return;
 
       adDiv.innerHTML = `
-        <a href="/click" target="_blank">
+        <a href="${API}/click" target="_blank">
           <img src="${ad.image}" width="300" height="250"/>
         </a>
       `;
