6c2b2d53518a68b7d903096c3adf1ac3b66f7151
xre4513-alt <xre4513@gmail.com>
2026-04-29 11:02:04 +0530

    ad.js

--- public/ad.js
+++ public/ad.js
@@ -1,14 +1,15 @@
 (function () {
   const API = "https://mini-ad-network.onrender.com";
+  const SITE_ID = "site1"; // change per website
 
-  fetch(API + "/getAd")
+  fetch(API + "/getAd?site=" + SITE_ID)
     .then(res => res.json())
     .then(ad => {
       const adDiv = document.getElementById("ad-slot");
       if (!adDiv) return;
 
       adDiv.innerHTML = `
-        <a href="${API}/click" target="_blank">
+        <a href="${API}/click?site=${SITE_ID}" target="_blank">
           <img src="${ad.image}" width="300" height="250"/>
         </a>
       `;
